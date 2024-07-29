71665276b886a49b906d3f7e011871d6ffab7192
Rajnish <virpaterajnish@gmail.com>
2024-07-29 16:15:10 +0530

    changed connection string to point to azure

--- FRONTEND/frontend/src/global.js
+++ FRONTEND/frontend/src/global.js
@@ -1,2 +1,4 @@
-export const connection_string =
-  "https://videoeditbackend-9ff0ec21f773.herokuapp.com/";
+// export const connection_string =
+//   "https://videoeditbackend-9ff0ec21f773.herokuapp.com/";
+
+export const connection_string = "https://videoeditbackend.azurewebsites.net/";
